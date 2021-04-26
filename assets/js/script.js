2b906fd2b83208a02770f16da61e80459d6b9d86
StefanoCeli <stefanopsn91@gmail.com>
2021-04-26 22:54:07 +0200

    inserito commenti

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-
+    //creo un oggetto studente con valori all'interno
     var studente = {
         nome : 'gianluca',
         cognome : 'rossi',
         età :'25'
     }
-
+    //ciclo for in per visualizzare le chiavi e i valori dell'oggetto
     for( var key in studente){
         console.log( key + ' :' + studente[key]);
     }
-
+    //creo un array di oggetti
     var studenti = [
         {
             nome : 'gianluca',
@@ -32,21 +32,22 @@ $(document).ready(function(){
             età :'18'
         }
     ]
-
+    //ciclo for of per visualizzare gli oggetti dentro l'array e all'interno vado a chiamare i valori di nome e cognome
     for(var studente of studenti){
         console.log( 'nome: ' + studente['nome'] + ' cognome: ' + studente['cognome']);
         
     }
-
+    //dichiaro varibili per chiedere all'utente i dati 
     var newName = prompt("inserisci un nome");
     var newSurname = prompt("inserisci un cognome");
     var newAge = parseInt(prompt("inserisci l'età"));
-
+    //richiamo la funzione che andrà a pushare i dati del prompt
     addStudente(studenti,newName,newSurname,newAge);
 
     console.log('---------------------------');
-    
+    //ciclo for of
     for(var studente of studenti){
+        //all'interno metto un ciclo for in per richiamare tutti i singoli dati
         for(var dati in studente){
             console.log(dati + ': ' + studente[dati]);
             console.log('---------------------------');
@@ -55,7 +56,7 @@ $(document).ready(function(){
     }
 
 })
-
+//funzione per pushare un nuovo oggetto in un array
 function addStudente(target,name,surname,age){
     target.push(
         {
